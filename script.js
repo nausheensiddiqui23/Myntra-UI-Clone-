9f11c121638c52c258dad42cd78145617dc5bdcf
Nausheen Siddiqui <nausheenzmr@gmail.com>
2025-08-25 01:51:47 +0530

    new commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example: Add product to cart
+document.querySelectorAll(".product-card").forEach(card => {
+  card.addEventListener("click", () => {
+    alert("Product added to cart 🛒");
+  });
+});
